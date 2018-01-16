d8039db9ecadc133d76791abfbaecc5ebfdcf39a
Shaomg <Shaomg.com>
2018-01-16 21:12:16 +0800

    添加管理员模块首界面

--- src/main/webapp/resources/js/edit.js
+++ src/main/webapp/resources/js/edit.js
@@ -1,14 +1,10 @@
 $(document).ready(function () {
 
-    /*$('#edit_save').text(FanYu.editSave);
-    $('#edit_issue').text(FanYu.editIssue);
-    $('#edit_left_top_h1').text(FanYu.editTit);
-    $('#edit_left_top_h3').text(FanYu.editHint);
-*/
     $('#edit_right_Tit_right_div').on('click', '#edit_save,#edit_issue', function () {
         switch ($(this).attr('id')) {
             case 'edit_save':
                 showDialog('保存');
+                // console.info(iframe.window.getEditContent());
                 break;
             case 'edit_issue':
                 showDialog('发布');
---
+++ src/main/webapp/resources/js/manage.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Shaomg on 2018/1/16.
+ */
+$(document).ready(function () {
+    $('#manageLogin_btn').click(function () {
+        $('#manage_content').fadeOut(500);
+        $('#content').fadeIn(500);
+    })
+});
